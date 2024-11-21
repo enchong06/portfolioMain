ae4464e2bda74dc0b120eedf65d2a696e47fd61a
3nchongDev <130216080+enchong06@users.noreply.github.com>
2024-11-21 23:53:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('github' && 'github-2')
+button.addEventListener('click', myfunction)
+    function myfunction(){
+        alert('Hello, Welcome to my Github!')
+    }
